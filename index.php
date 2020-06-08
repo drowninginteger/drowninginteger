415bd9a7464906652193eaccead2bfea75d7aff5
Connor Blikre <connorb@jordancrown.com>
2020-06-07 17:27:40 -0700

    added project features

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="https://www.chambersusa.com/" target="_blank">https://www.chambersusa.com/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
 
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Search-by-state client list with interactive US map</li>
+								<li>Filter by category/state project list</li>
+								<li>Club Road Magazine Issuu Viewer</li>
+								<li>Interactive company history timeline</li>
+								<li>Event calendar</li>
+							</ul>
+						</div>
+
 						<img src="images/projects/chambers/chambers-4-3-large.jpg" class="site-image-large" />
 
 						<?php $url = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
@@ -45,6 +57,16 @@
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="https://www.gamut360.com/" target="_blank">https://www.gamut360.com/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
 
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Property listings with interactive map</li>
+								<li>Careers page and application form</li>
+								<li>Instagram feed</li>
+							</ul>
+						</div>
+
 						<img src="images/projects/gamut/gamut-4-3-large.jpg" class="site-image-large" />
 
 						<?php $url = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
@@ -67,6 +89,15 @@
 						<h2 class="title">PowerA Video Game Accessories</h2>
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="https://www.powera.com/" target="_blank">https://www.powera.com/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
+
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Product search by type, platform, and brand</li>
+								<li>News page</li>
+							</ul>
+						</div>
 
 						<img src="images/projects/powera/powera-4-3-large.jpg" class="site-image-large" />
 
@@ -92,6 +123,16 @@
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="https://mmbnetworks.com/" target="_blank">https://mmbnetworks.com/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
 
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Products list</li>
+								<li>Services list</li>
+								<li>Knowledge base</li>
+							</ul>
+						</div>
+
 						<img src="images/projects/mmb/mmb-4-3-large.jpg" class="site-image-large" />
 
 						<?php $url = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
@@ -115,6 +156,17 @@
 						<h2 class="title">Exterior Crew Siding</h2>
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="https://www.exteriorcrew.com/" target="_blank">https://www.exteriorcrew.com/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
+
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Project portfolio</li>
+								<li>Page section navigation menu</li>
+								<li>Services page</li>
+								<li>Testimonials page</li>
+							</ul>
+						</div>
 
 						<img src="images/projects/exteriorcrew/exteriorcrew-4-3-large.jpg" class="site-image-large" />
 
@@ -140,6 +192,16 @@
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="http://www.imaginecm.org/" target="_blank">http://www.imaginecm.org/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
 
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Program directory</li>
+								<li>Event calendar</li>
+								<li>Donation forms integrated with Paypal</li>
+							</ul>
+						</div>
+
 						<img src="images/projects/imagine/imagine-4-3-large.jpg" class="site-image-large" />
 
 						<?php $url = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
@@ -164,6 +226,17 @@
 						<p class="role"><span class="role-tag">Development</span></p>
 						<p><a href="https://nationalfund.org/" target="_blank">https://nationalfund.org/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
 
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Regional collaboratives directory</li>
+								<li>Publications directory</li>
+								<li>News page, including a blog and social media aggregate feed</li>
+								<li>Client returned to us for several large site updates over several years</li>
+							</ul>
+						</div>
+
 						<img src="images/projects/nfws/nfws-4-3-large.jpg" class="site-image-large" />
 
 						<?php $url = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"; ?>
@@ -187,6 +260,16 @@
 						<h2 class="title">Jet City Roller Derby</h2>
 						<p class="role"><span class="role-tag">Design</span> <span class="role-tag">Development</span></p>
 						<p><a href="https://jetcityrollerderby.com/" target="_blank">https://jetcityrollerderby.com/ &nbsp; <i class="fas fa-external-link-alt"></i></a></p>
+
+						<div class="overview">
+							<h3>Key Features</h3>
+
+							<ul>
+								<li>Skater directory</li>
+								<li>Blog</li>
+								<li>Existing site design modernized by me</li>
+							</ul>
+						</div>
 
 						<img src="images/projects/jcrd/jcrd-4-3-large.jpg" class="site-image-large" />
 
